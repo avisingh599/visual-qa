c09f70b6611e382485f74d9d5f6f1ef8288a4e35
Avi Singh <avisingh599@gmail.com>
2015-10-26 12:27:48 +0530

    fixes to timeseries features

--- scripts/features.py
+++ scripts/features.py
@@ -26,13 +26,25 @@ def question2VecSum(nlp, q, featureDim):
 
 	return outputvec
 
-def computeVectors(qu,an,img,VGGfeatures,nlp,img_map,encoder,nb_classes,timeSeries = False):
+def computeVectors(qu,an,img,VGGfeatures,nlp,img_map,encoder,nb_classes):
 	features = np.zeros((1,4096+300))
 	
 	features[0,0:300] = question2VecSum(nlp,qu,300)
 	features[0,300:] = VGGfeatures[:,img_map[img]]
 
 	return features
+
+def computeVectorsTimeSeries(qu,an,img,VGGfeatures,nlp,img_map,encoder,nb_classes, maxLen):
+	featureDim = 300
+	img_dim = 4096
+	features_q = np.zeros((1, maxLen, featureDim))
+	features_i = np.zeros((1, img_dim))
+
+	features_q[0,:,:] = question2VecSum(nlp,qu,300)
+	features_i[0,:] = VGGfeatures[:,img_map[img]]
+
+	return features_i,features_q
+
 
 def computeVectorsBatch(qu,an,img,VGGfeatures,nlp,img_map,encoder,nb_classes):
 	word2vecDim = 300
