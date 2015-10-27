2f8fe540a462c61fc23f762dc2578e59e90833e1
Avi Singh <avisingh599@gmail.com>
2015-10-28 02:38:10 +0530

    added argparse to evaluateModel

--- scripts/evaluateModel.py
+++ scripts/evaluateModel.py
@@ -1,32 +1,40 @@
 import sys
-from keras.models import model_from_json, Sequential
-from q2v import question2VecSum
+import argparse
+
+from keras.models import model_from_json
 
 from spacy.en import English
 import numpy as np
 import scipy.io
 from sklearn.externals import joblib
 
-from features import computeVectors
+from features import computeVectors, computeVectorsTimeSeries
 
 
 if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-model', type=str, required=True)
+	parser.add_argument('-weights', type=str, required=True)
+	parser.add_argument('-results', type=str, required=True)
+	args = parser.parse_args()
+
+	model = model_from_json(open(args.model).read())
+	model.load_weights(args.weights)
+	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
 	questions_val = open('../data/preprocessed/questions_val2014.txt', 'r').read().decode('utf8').splitlines()
 	answers_val = open('../data/preprocessed/answers_val2014.txt', 'r').read().decode('utf8').splitlines()
 	images_val = open('../data/preprocessed/images_val2014.txt', 'r').read().decode('utf8').splitlines()
 	vgg_model_path = '../features/coco/vgg_feats.mat'
-
-	model = model_from_json(open('../model/configs/mlp_full_numHiddenUnits_1000.json').read())
-	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-	model.load_weights('/home/avisingh/Datasets/vqa/neuralmodels/mlp_epoch_1_loss_2.1888983132.hdf5')
+	
 	print 'Model compiled, weights loaded...'
-	labelencoder = joblib.load('labelencoder.pkl')
+	labelencoder = joblib.load('../models/labelencoder.pkl')
 
 	features_struct = scipy.io.loadmat(vgg_model_path)
 	VGGfeatures = features_struct['feats']
 	print 'loaded vgg features'
-	image_ids = open('/home/avisingh/Datasets/vqa/preprocessed/coco_vgg_IDMap.txt').read().splitlines()
+	image_ids = open('../features/coco/coco_vgg_IDMap.txt').read().splitlines()
 	img_map = {}
 	for ids in image_ids:
 		id_split = ids.split()
@@ -38,16 +46,22 @@ if __name__ == "__main__":
 	nb_classes = 1000
 	y_predict_text = []
 	for q,an,im,i in zip(questions_val,answers_val,images_val,xrange(len(questions_val))):
-		x = computeVectors(q,an,im,VGGfeatures, nlp, img_map, labelencoder, nb_classes)
+		if args.model.find('mlp') == 1:
+			x = computeVectors(q,an,im,VGGfeatures, nlp, img_map, labelencoder, nb_classes)
+		elif args.model.find('lstm') == 1:
+			x = computeVectorsTimeSeries(q,an,im,VGGfeatures, nlp, img_map, labelencoder, nb_classes)
+		else:
+			Exception('Unknown model type. Please check your model file')
+
 		y_predict = model.predict_classes(x, verbose=0)
-		y_predict_text.append(labelencoder.inverse_transform(y_predict))
+		y_predict_text.append(labelencoder.inverse_transform(y_predict[-1]))
 		#print y_predict_text[-1]
 		if i%100 == 0:
 			print i
 
 	correct_val=0
 	incorrect_val=0
-	f1 = open('predictions_mlp_1000.txt', 'w')
+	f1 = open(args.results, 'w')
 
 	for prediction,truth in zip(y_predict_text, answers_val):
 		temp_count=0
@@ -63,6 +77,7 @@ if __name__ == "__main__":
 		f1.write(prediction[-1])
 		f1.write('\n')
 
+	f1.write(str(float(correct_val)/(incorrect_val+correct_val)))
 	print float(correct_val)/(incorrect_val+correct_val)
 
 
