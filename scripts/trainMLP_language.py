90f006189a5899b2d0523979a2e86c4940c30ee2
Avi Singh <avisingh599@gmail.com>
2015-10-29 14:16:55 +0530

    Add language only model

--- scripts/trainMLP_language.py
+++ scripts/trainMLP_language.py
@@ -3,11 +3,9 @@ from random import shuffle
 import argparse
 
 import numpy as np
-import scipy.io
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD
 from keras.utils import np_utils, generic_utils
 
 from sklearn import preprocessing
@@ -15,11 +13,10 @@ from sklearn.externals import joblib
 
 from spacy.en import English
 
-from features import computeVectorsBatch
+from features import computeLanguageVectorsBatch
 from utils import grouper, selectFrequentAnswers
 
 def main():
-
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-num_hidden_units', type=int, default=1024)
 	parser.add_argument('-num_hidden_layers', type=int, default=2)
@@ -39,15 +36,11 @@ def main():
 	labelencoder.fit(answers_train)
 	nb_classes = len(list(labelencoder.classes_))
 	joblib.dump(labelencoder,'../models/labelencoder.pkl')
-	#y_train = le.transform(answers_train)
-	#Y_train = np_utils.to_categorical(y_train, nb_classes)
 
-	#define model, in this case an MLP
-	img_dim = 4096
 	word_vec_dim = 300
 	model = Sequential()
 	for i in xrange(args.num_hidden_layers):
-		model.add(Dense(args.num_hidden_units, input_dim=img_dim+word_vec_dim, init='uniform'))
+		model.add(Dense(args.num_hidden_units, input_dim=word_vec_dim, init='uniform'))
 		model.add(Activation(args.activation))
 		model.add(Dropout(args.dropout))
 
@@ -55,22 +48,14 @@ def main():
 	model.add(Activation('softmax'))
 
 	json_string = model.to_json()
-	model_file_name = '../models/mlp_num_hidden_units_' + str(args.num_hidden_units) + '_num_hidden_layers_' + str(args.num_hidden_layers)
+	model_file_name = '../models/mlp_language_num_hidden_units_' + str(args.num_hidden_units) + '_num_hidden_layers_' + str(args.num_hidden_layers)
 	open(model_file_name  + '.json', 'w').write(json_string)
-
+	
 	print 'Compiling model...'
 	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 	print 'Compilation done...'
-	#set up CNN features and word vectors
-	features_struct = scipy.io.loadmat(vgg_model_path)
-	VGGfeatures = features_struct['feats']
-	print 'loaded vgg features'
-	image_ids = open('../features/coco/coco_vgg_IDMap.txt').read().splitlines()
-	id_map = {}
-	for ids in image_ids:
-		id_split = ids.split()
-		id_map[id_split[0]] = int(id_split[1])
 
+	#set up word vectors
 	nlp = English()
 	print 'loaded word2vec features...'
 	## training
@@ -83,10 +68,9 @@ def main():
 		shuffle(index_shuf)
 		questions_train = [questions_train[i] for i in index_shuf]
 		answers_train = [answers_train[i] for i in index_shuf]
-		images_train = [images_train[i] for i in index_shuf]
 		progbar = generic_utils.Progbar(len(questions_train))
-		for qu,an,im in zip(grouper(questions_train, batchSize, fillvalue=questions_train[0]), grouper(answers_train, batchSize, fillvalue=answers_train[0]), grouper(images_train, batchSize, fillvalue=images_train[0])):
-			X_batch, Y_batch = computeVectorsBatch(qu,an,im,VGGfeatures,nlp,id_map,labelencoder,nb_classes)
+		for qu,an in zip(grouper(questions_train, batchSize, fillvalue=questions_train[0]), grouper(answers_train, batchSize, fillvalue=answers_train[0])):
+			X_batch, Y_batch = computeLanguageVectorsBatch(qu,an,nlp,labelencoder,nb_classes)
 			loss = model.train_on_batch(X_batch, Y_batch)
 			progbar.add(batchSize, values=[("train loss", loss)])
 		#print type(loss)
