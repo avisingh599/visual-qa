96649db7d22fb13ff329fffad96f8feab469f126
Avi Singh <avisingh599@gmail.com>
2015-10-30 17:07:24 +0530

    Add progresbarr, question lengths

--- scripts/dumpText.py
+++ scripts/dumpText.py
@@ -2,6 +2,9 @@ import operator
 import argparse
 import sys
 import os
+import progressbar
+
+from spacy.en import English
 
 if os.path.isdir('../3rdParty/VQA/PythonHelperTools/vqaTools/'):
 	sys.path.insert(0, '../3rdParty/VQA/PythonHelperTools/vqaTools/')
@@ -26,16 +29,18 @@ def getAllAnswer(answers):
 
 	return ';'.join(answer_list)
 
-if __name__ == "__main__":
-
+def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-isTrain', type=int, default=1)
 	args = parser.parse_args()
+
+	nlp = English() #used for conting number of tokens
 
 	if args.isTrain == 1:
 		annFile = '../data/Annotations_Train_mscoco/mscoco_train2014_annotations.json'
 		quesFile = '../data/Questions_Train_mscoco/OpenEnded_mscoco_train2014_questions.json'
 		questions_file = open('../data/preprocessed/questions_train2014.txt', 'w')
+		questions_lengths_file = open('../data/preprocessed/questions_lengths_train2014.txt', 'w')
 		answers_file = open('../data/preprocessed/answers_train2014.txt', 'w')
 		coco_image_id = open('../data/preprocessed/images_train2014.txt', 'w')
 		trainval = 'training data'
@@ -43,6 +48,7 @@ if __name__ == "__main__":
 		annFile = '../data/Annotations_Val_mscoco/mscoco_val2014_annotations.json'
 		quesFile = '../data/Questions_Val_mscoco/OpenEnded_mscoco_val2014_questions.json'
 		questions_file = open('../data/preprocessed/questions_val2014.txt', 'w')
+		questions_lengths_file = open('../data/preprocessed/questions_lengths_val2014.txt', 'w')
 		answers_file = open('../data/preprocessed/answers_val2014.txt', 'w')
 		coco_image_id = open('../data/preprocessed/images_val2014.txt', 'w')
 		trainval = 'validation data'
@@ -51,11 +57,17 @@ if __name__ == "__main__":
 	vqa=VQA(annFile, quesFile)
 	questions = vqa.questions
 	ques = questions['questions']
-	qa = vqa.qa;
+	qa = vqa.qa
 
-	for i, q in zip(xrange(1,len(ques)+1),ques):
+	
+	pbar = progressbar.ProgressBar()
+	print 'Dumping questions,answers, imageIDs, and questions lenghts to text files...'
+	for i, q in pbar(zip(xrange(1,len(ques)+1),ques)):
 		questions_file.write(q['question'].encode('utf8'))
 		questions_file.write('\n'.encode('utf8'))
+		questions_lengths_file.write(str(len(nlp(q['question']))).encode('utf8'))
+		questions_lengths_file.write('\n'.encode('utf8'))
+
 		coco_image_id.write(str(q['image_id']).encode('utf8'))
 		coco_image_id.write('\n')
 		if args.isTrain:
@@ -65,3 +77,6 @@ if __name__ == "__main__":
 		answers_file.write('\n'.encode('utf8'))
 
 	print 'completed dumping', trainval
+
+if __name__ == "__main__":
+	main()
